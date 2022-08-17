9fc6ada3d8471abfcfe00a29ce2b7b5c0184bff0
zrah <zrah@protonmail.com>
2022-08-18 02:15:52 +0800

    chore: remove unused packages

--- shell.nix
+++ shell.nix
@@ -2,8 +2,5 @@ let nixpkgs = import <nixpkgs> { };
 in with nixpkgs;
 stdenv.mkDerivation {
   name = "rust";
-  buildInputs =
-    [ pkgconfig openssl.dev rustup cargo-watch zlib.dev nixfmt gnumake sqlite ];
-  OPENSSL_DEV = openssl.dev;
-  ZLIB_DEV = zlib.dev;
+  buildInputs = [ rustup cargo-watch bintools clang ];
 }
